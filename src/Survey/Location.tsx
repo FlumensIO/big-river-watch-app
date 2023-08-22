66337a22df7d06a5a6629dcab1dbed87eda0be2c
Karolis Kazlauskis <karolis@flumens.io>
2023-08-22 13:28:36 +0300

    Clean up

--- src/Survey/Location.tsx
+++ src/Survey/Location.tsx
@@ -90,6 +90,7 @@ const Location = ({ sample: record }: Props) => {
   const [mapRef, setMapRef] = useState<MapRef>();
   const flyToLocation = () => mapFlyToLocation(mapRef, location);
   useEffect(flyToLocation, [mapRef, location]);
+
   return (
     <Page id="survey-location">
       <Header />
