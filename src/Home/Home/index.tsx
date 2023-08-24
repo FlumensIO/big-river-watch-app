fcd86e7ac501c6f3408c647cdef2d673fefdd64d
Karolis Kazlauskis <karolis@flumens.io>
2023-08-24 10:27:11 +0300

    Update home page background

--- src/Home/Home/index.tsx
+++ src/Home/Home/index.tsx
@@ -13,7 +13,7 @@ const HomeController = () => {
         <IonItemGroup>
           <IonRouterLink routerLink="/survey" routerDirection="none">
             <IonButton
-              className="absolute bottom-20 left-1/2 h-14 w-3/4 -translate-x-1/2 -translate-y-1/2 transform rounded-md text-center text-lg font-bold shadow-2xl [--border-radius:15px]"
+              className="absolute bottom-20 left-1/2 h-14 w-3/4 -translate-x-1/2 -translate-y-1/2 transform rounded-md text-center text-lg font-semibold shadow-2xl [--border-radius:15px]"
               color="primary"
               mode="ios"
             >
