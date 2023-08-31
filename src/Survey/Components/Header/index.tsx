cd4e8a2bbaad2897f8107b85b8d036e321a7d373
Karolis Kazlauskis <karolis@flumens.io>
2023-08-31 22:09:45 +0300

    Add option to leave survey

--- src/Survey/Components/Header/index.tsx
+++ src/Survey/Components/Header/index.tsx
@@ -1,7 +1,9 @@
-import { FC, useState } from 'react';
+import { useContext, useState } from 'react';
 import { observer } from 'mobx-react';
-import { Trans as T } from 'react-i18next';
+import { closeOutline } from 'ionicons/icons';
+import { Trans as T, useTranslation } from 'react-i18next';
 import { useRouteMatch } from 'react-router';
+import { useAlert } from '@flumens';
 import {
   IonHeader,
   IonToolbar,
@@ -10,21 +12,59 @@ import {
   IonProgressBar,
   useIonRouter,
   useIonViewWillEnter,
+  IonBackButton,
+  IonButton,
+  IonIcon,
+  NavContext,
 } from '@ionic/react';
+import records from 'models/collections/records';
+import Record from 'models/record';
 import { questionRoutes } from '../../router';
-import BackButton from './BackButton';
 import './styles.scss';
 
-type Props = {
-  onCancel?: any;
-  rightSlot?: any;
+const useCancelPrompt = () => {
+  const alert = useAlert();
+
+  const showCancelPropt = () => {
+    const promiseWrap = (resolve: (value: unknown) => void): void => {
+      alert({
+        header: 'Exit',
+        message:
+          'Do you want to close and save the survey for later or discard it?',
+        buttons: [
+          {
+            text: 'Discard',
+            role: 'destructive',
+            handler: () => resolve(true),
+          },
+          {
+            text: 'Close',
+            role: 'cancel',
+            handler: () => resolve(false),
+          },
+        ],
+      });
+    };
+    return new Promise(promiseWrap);
+  };
+
+  return showCancelPropt;
 };
 
-const SurveyHeader: FC<Props> = ({ onCancel, rightSlot }) => {
-  const match = useRouteMatch();
+const SurveyHeader = () => {
+  const match = useRouteMatch<{ smpId: string }>();
+
+  const byId = (record: Record) => record.cid === match.params.smpId;
+  const record = records.find(byId);
+
   const surveyProgressIndex = parseInt(match.url.split('/').pop()!, 10);
 
+  const { navigate } = useContext(NavContext);
+
   const router = useIonRouter();
+
+  const { t } = useTranslation();
+  const showCancelPropt = useCancelPrompt();
 
   const backButtonLabel = router.routeInfo.routeOptions?.comingFrom || 'Back';
 
@@ -40,11 +80,23 @@ const SurveyHeader: FC<Props> = ({ onCancel, rightSlot }) => {
   const updateIndex = () => setIndex(surveyProgressIndex);
   useIonViewWillEnter(updateIndex);
 
+  const onCancel = async () => {
+    const discard = await showCancelPropt();
+    if (discard) {
+      record.destroy();
+    }
+
+    navigate('/home/landing', 'root');
+  };
+
   return (
     <IonHeader id="survey-header">
       <IonToolbar>
-        <IonButtons onClick={onCancel} slot="start">
-          <BackButton onCancel={onCancel} backButtonLabel={backButtonLabel} />
+        <IonButtons slot="start">
+          <IonBackButton
+            text={t(backButtonLabel)}
+            defaultHref="/home/landing"
+          />
         </IonButtons>
 
         <IonTitle>
@@ -54,9 +106,13 @@ const SurveyHeader: FC<Props> = ({ onCancel, rightSlot }) => {
           </span>
         </IonTitle>
 
-        <IonProgressBar color="secondary" value={index / surveyStepCount} />
+        <IonButtons slot="end">
+          <IonButton onClick={onCancel}>
+            <IonIcon icon={closeOutline} slot="icon-only" />
+          </IonButton>
+        </IonButtons>
 
-        {rightSlot}
+        <IonProgressBar color="secondary" value={index / surveyStepCount} />
       </IonToolbar>
     </IonHeader>
   );
