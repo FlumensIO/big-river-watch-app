21ce5cd420283cc7e3578f1eb98274399280f768
Karolis Kazlauskis <karolis@flumens.io>
2024-04-19 18:04:58 +0300

    Update backup script

--- other/backup.js
+++ other/backup.js
@@ -18,13 +18,27 @@ const date = new Date().toISOString().split('T')[0].replaceAll('-', '_');
 console.log(`Doing ${date} backup`);
 
 const run = async () => {
-  console.log('Fetching files list');
+  const { data } = await supabase
+    .from('config')
+    .select('value')
+    .filter('key', 'eq', 'backup');
+  const { value: oldOffset } = data[0];
 
-  const { data: files } = await supabase.storage.from('media').list('public');
+  const offset = parseInt(process.env.OFFSET || oldOffset, 10);
+
+  console.log('Fetching files list, offset=', offset);
+
+  const { data: files } = await supabase.storage.from('media').list('public', {
+    limit: 10000,
+    offset,
+    sortBy: { column: 'created_at', order: 'asc' },
+  });
+
   console.log(`Found ${files?.length} files`);
 
-  for (const file of files) {
-    process.stdout.write(`${file.name.split('-')[0]} ↓`);
+  for (const [index, file] of files?.entries()) {
+    process.stdout.write(`${offset + index}. ${file.name.split('-')[0]} ↓`);
+
     const { data } = await supabase.storage
       .from('media')
       .download(`public/${file.name}`);
@@ -36,6 +50,11 @@ const run = async () => {
       file.name,
       `org.theriverstrust.bigriverwatch/backup/storage`
     );
+
+    await supabase
+      .from('config')
+      .update({ value: offset + index })
+      .filter('key', 'eq', 'backup');
 
     process.stdout.write(' (✓)\n');
   }
